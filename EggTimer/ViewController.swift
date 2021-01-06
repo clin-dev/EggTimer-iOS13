af8cf4e893c0494940f120d8e3dc8bf129f43ef9
clin-dev <chaolinle@gmail.com>
2021-01-05 22:43:16 -0500

    Added count down timer. Still need to implement progress bar

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    let eggTimes = ["Soft": 180, "Medium": 420, "Hard": 720] //in seconds
+    var counter = 0;
+    var timer = Timer();
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        counter = eggTimes[sender.currentTitle!]!
+        resetTimer()
+    }
+    
+    @objc func updateCounter() {
+        if counter > 0 {
+            print("\(counter) seconds")
+            counter -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "DONE!"
+        }
+    }
+    
+    func startTimer(){
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
 
-
+    func resetTimer(){
+        timer.invalidate()
+        startTimer()
+    }
+    
 }
